de89cfeeb46cb4f9ca90b0255cddb844fa06636e
zhouzaida <zhouzaida@163.com>
2021-05-24 19:10:44 +0800

    remove get_commitid function

--- mim/utils/utils.py
+++ mim/utils/utils.py
@@ -223,17 +223,6 @@ def is_version_equal(version1: str, version2: str) -> bool:
     return LooseVersion(version1) == LooseVersion(version2)
 
 
-def get_commit_id(package: str) -> str:
-    """Get commit id from package.
-
-    Args:
-        package (str): Name of package.
-    """
-    target_pkg = PKG_ALIAS.get(package, package)
-    module = importlib.import_module('..commit_id', f'{target_pkg}.subpkg')
-    return module.commit_id  # type: ignore
-
-
 def get_installed_path(package: str) -> str:
     """Get installed path of package.
 
