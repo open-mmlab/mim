f7f69cbd51b082277994f418b319a96b64497739
zhouzaida <zhouzaida@163.com>
2021-05-25 22:40:17 +0800

    add README.md in package info

--- setup.py
+++ setup.py
@@ -1,5 +1,12 @@
 from setuptools import find_packages, setup  # type: ignore
 
+
+def readme():
+    with open('README.md', encoding='utf-8') as f:
+        content = f.read()
+    return content
+
+
 version_file = 'mim/version.py'
 
 
@@ -13,11 +20,13 @@ setup(
     name='openmim',
     version=get_version(),
     description='MIM Installs OpenMMLab packages',
+    long_description=readme(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/open-mmlab/mim',
     author='MIM Authors',
     author_email='openmmlab@gmail.com',
     packages=find_packages(),
     include_package_data=True,
-    url='https://github.com/open-mmlab/mim',
     python_requires='>=3.6',
     install_requires=[
         'Click==7.1.2',
