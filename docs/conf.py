752524d9366c59ae166fcdcc235368b4e0175f28
zhouzaida <zhouzaida@163.com>
2021-12-10 10:49:12 +0800

    delete logo

--- docs/conf.py
+++ docs/conf.py
@@ -83,7 +83,6 @@ html_theme_options = {
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
-html_css_files = ['css/readthedocs.css']
 
 # -- Extension configuration -------------------------------------------------
 # Ignore >>> when copying code
