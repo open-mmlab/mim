9b2d8e4b5800c467466c295a98aec1235b9b61d5
ice-tong <xych6@outlook.com>
2022-06-23 12:33:26 +0800

    combine commits: for ci debug

--- tests/test_uninstall.py
+++ tests/test_uninstall.py
@@ -1,4 +1,7 @@
 # Copyright (c) OpenMMLab. All rights reserved.
+import traceback
+
+import pip
 from click.testing import CliRunner
 
 from mim.commands.install import cli as install
@@ -17,39 +20,47 @@ def setup_module():
 def test_uninstall():
     runner = CliRunner()
 
-    # mim install mmdet mmsegmentation
-    result = runner.invoke(install, ['mmdet', 'mmsegmentation', '--yes'])
+    import site
+    import subprocess
+    subprocess.check_call(['pip', 'list'])
+    subprocess.check_call(['ls', site.getsitepackages()[0]])
+    subprocess.check_call(['pip', 'freeze'])
+    print(site.getsitepackages())
+
+    # mim install mmsegmentation --yes
+    result = runner.invoke(install, ['mmsegmentation', '--yes'])
+    traceback.print_exception(*result.exc_info)
+    print(result.output)
+    print(pip.__version__)
     assert result.exit_code == 0
 
     # check if install success
     result = list_package()
     installed_packages = [item[0] for item in result]
-    assert 'mmdet' in installed_packages
     assert 'mmsegmentation' in installed_packages
     assert 'mmcv-full' in installed_packages
     # `mim install mmsegmentation` will install mim extra requirements (via
     # mminstall.txt) automatically since PR#132, so we got installed mmcls here.  # noqa: E501
     assert 'mmcls' in installed_packages
 
-    # mim uninstall mmdet mmsegmentation
-    result = runner.invoke(uninstall, ['mmdet', 'mmsegmentation', '--yes'])
+    # mim uninstall mmsegmentation --yes
+    result = runner.invoke(uninstall, ['mmsegmentation', '--yes'])
     assert result.exit_code == 0
 
     # check if uninstall success
     result = list_package()
     installed_packages = [item[0] for item in result]
-    assert 'mmdet' not in installed_packages
     assert 'mmsegmentation' not in installed_packages
 
-    # mim uninstall mmcls mmcv-full
-    result = runner.invoke(uninstall, ['mmcv-full', 'mmmcls', '--yes'])
+    # mim uninstall mmcls mmcv-full --yes
+    result = runner.invoke(uninstall, ['mmcls', 'mmcv-full', '--yes'])
     assert result.exit_code == 0
 
     # check if uninstall success
     result = list_package()
     installed_packages = [item[0] for item in result]
-    assert 'mmdet' not in installed_packages
-    assert 'mmsegmentation' not in installed_packages
+    assert 'mmcls' not in installed_packages
+    assert 'mmcv-full' not in installed_packages
 
 
 def teardown_module():
