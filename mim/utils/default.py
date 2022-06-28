7bd741c11e5f9c578f999469781c0b2c6d949745
ice-tong <xych6@outlook.com>
2022-06-28 16:56:26 +0800

    feat(mim install): support specify MMCV_FIND_BASE_URL env variable

--- mim/utils/default.py
+++ mim/utils/default.py
@@ -5,6 +5,8 @@ import os.path as osp
 USER = 'open-mmlab'
 DEFAULT_URL = f'https://github.com/{USER}'
 
+DEFAULT_MMCV_FIND_BASE_URL = 'https://download.openmmlab.com'
+
 WHEEL_URL = {
     'mmcv-full':
     'https://download.openmmlab.com/mmcv/dist/{cuda_version}/'
