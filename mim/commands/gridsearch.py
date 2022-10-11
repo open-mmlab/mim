8066832482c249cb36baf0acbb19ef159004f424
Suraj Pattar <pattarsuraj@gmail.com>
2022-10-11 15:34:04 +0900

    Refine error message to show how to install mmengine. (#161)
    * :zap: Split error msg in two lines.
    
    * :zap: Remove bracket.
    
    * :zap: Remove triple quotes.

--- mim/commands/gridsearch.py
+++ mim/commands/gridsearch.py
@@ -276,7 +276,8 @@ def gridsearch(
             from mmcv import Config
         except ImportError:
             raise ImportError(
-                'Please install mmengine to use the gridsearch command!')
+                'Please install mmengine to use the gridsearch command: '
+                '`mim install mmengine`.')
 
     cfg = Config.fromfile(config)
     for arg in search_args_dict:
