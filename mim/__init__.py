fe1e670e4502dd64f42292df4745d05b6d355bf8
yancong <xych6@outlook.com>
2023-02-03 18:12:56 +0800

    fix AssertionError when importing pip before setuptools

--- mim/__init__.py
+++ mim/__init__.py
@@ -1,4 +1,18 @@
 # Copyright (c) OpenMMLab. All rights reserved.
+
+# NOTE: Since we could got AssertionError when importing pip before
+# setuptools. A workaround is to import setuptools first and filter
+# warnings that cased by setuptools replacing distutils.
+# Related issues:
+# - https://github.com/pypa/setuptools/issues/3621
+# - https://github.com/open-mmlab/mmclassification/issues/1343
+try:
+    import setuptools  # noqa: F401
+    import warnings
+    warnings.filterwarnings('ignore', 'Setuptools is replacing distutils')
+except ImportError:
+    pass
+
 from .commands import (
     download,
     get_model_info,
