f2d9fef0c650b52d7f9a1431375cb98d92c666a6
yancong <xych6@outlook.com>
2023-02-03 18:17:16 +0800

    refine comments

--- mim/__init__.py
+++ mim/__init__.py
@@ -1,8 +1,8 @@
 # Copyright (c) OpenMMLab. All rights reserved.
 
-# NOTE: Since we could got AssertionError when importing pip before
+# NOTE: We could got AssertionError when importing pip before
 # setuptools. A workaround is to import setuptools first and filter
-# warnings that cased by setuptools replacing distutils.
+# warnings that are caused by setuptools replacing distutils.
 # Related issues:
 # - https://github.com/pypa/setuptools/issues/3621
 # - https://github.com/open-mmlab/mmclassification/issues/1343
